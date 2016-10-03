b1c5cd9dd153a767409a5c82e04415e1043ba07d
pallav dubey <pallavdubey@gmail.com>
2016-10-03 19:49:05 +0530

    Base application

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lifePlannerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the lifePlannerApp
+ */
+angular.module('lifePlannerApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lifePlannerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lifePlannerApp
+ */
+angular.module('lifePlannerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
